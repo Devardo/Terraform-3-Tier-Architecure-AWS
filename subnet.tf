71937598a60bd5908ad9f37fc973a408ab9a39b4
devardo <devardostubbs@hotmail.com>
2024-05-08 12:28:59 -0400

    tf files for vpc, subnet, and variables added

---
+++ subnet.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.cidr[count.index]
+  availability_zone = var.az[count.index]
+  count             = 2
+
+  tags = {
+    Name = "public-sub"
+  }
+
+}
+
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1"
+  count             = 2
+
+  tags = {
+    Name = "private-sub3"
+  }
+
+}
